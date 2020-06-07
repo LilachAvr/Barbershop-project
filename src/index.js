1148805496c50facd87d64f23a6f0179fa70c513
לילך אברהם <lilachavr123@gmail.com>
2020-06-07 21:00:32 +0300

    Changed Port

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const app = require('./app')
 require('./datebase')
-
+const PORT = process.env.PORT || 5000
 
 async function init(){
-  await app.listen(2000);
+  await app.listen(PORT);
   console.log('Server on port 2000');
   
 }
