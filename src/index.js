f5653b231f705d2e44d052d9057a487ceb81dde9
לילך אברהם <lilachavr123@gmail.com>
2020-06-08 09:39:04 +0300

    ready for heroku 1

--- src/index.js
+++ src/index.js
@@ -2,14 +2,21 @@
 require('./datebase')
 const PORT = process.env.PORT || 2000
 
-async function init(){
-  await app.listen(PORT);
-  console.log('Server on port 2000');
+// async function init(){
+//   await app.listen(PORT);
+//   console.log('Server on port 2000');
   
-}
+// }
 const express = require('express');
 const app = express();
 const path = require('path');
+
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/my_barbershop",
+ { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false }
+  ).then(db => console.log('Datebase is Connected!'))
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
@@ -17,5 +24,6 @@ app.use(express.urlencoded({extended: false}));
 app.use(require('./controllers/authController'));
 
 app.use(express.static(path.join(__dirname, "pictureUser")))
-init();
+app.listen(PORT,console.log(`Server is starting at ${PORT}`))
+// init();
 
